326476b7764d93c5fa77d13f7ab12936a2b4dc56
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-08-25 12:37:20 -0400

    Add docstrings and formats to models, admin files

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
+"""
+Admin Configuration for Store Models
 
-# Register your models here.
+This module contains the admin configurations for the Store app models.
+"""
+
+from django.contrib import admin
+from .models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Product model.
+    """
+    list_display = ('title', 'category', 'price')
+    ordering = ('title',)
+    list_filter = ('category',)
+    search_fields = ('title', 'category__title')
--- products/models.py
+++ products/models.py
@@ -1,47 +1,114 @@
-from django.db import models
+"""
+Models for the Store App
 
+This module contains the Django models used for representing products, categories,
+currency types, images, and other entities in the store application.
+"""
+
+from django.db import models
+from django.contrib.auth.decorators import login_required
+#from django.utils.decorators import method_decorator
 
 class Category(models.Model):
+    """
+    Model representing a Category in the store.
+    """
     category = models.CharField(max_length=25, unique=True)
     description = models.CharField(max_length=250)
     show = models.BooleanField(default=True)
 
+    def __str__(self):
+        """
+        Returns the name of the category as its string representation.
+        """
+        return str(self.category)
+
     class Meta:
+        """
+        Adds extra metadata to the Category model.
+        """
         verbose_name = "Category"
         verbose_name_plural = "Categories"
 
-    def __str__(self):
-        return self.category
-
 
 class Image(models.Model):
+    """
+    Model representing a list of images in the products.
+    """
     link = models.URLField()
 
+    def __str__(self):
+        """
+        Returns the link of the image as its string representation.
+        """
+        return str(self.link)
+
     class Meta:
+        """
+        Adds extra metadata to the Image model.
+        """
         verbose_name = "Image"
         verbose_name_plural = "Images"
 
 
 class Currency(models.Model):
+    """
+    Model representing currency types in the store.
+    """
     name = models.CharField(max_length=50, unique=True)
+    symbol = models.CharField(max_length=10)
 
     def __str__(self):
-        return self.name
+        """
+        Returns the name of the currency as its string representation.
+        """
+        return str(self.name)
+
+    class Meta:
+        """
+        Adds extra metadata to the currency model.
+        """
+        verbose_name = "Currency"
+        verbose_name_plural = "Currencies"
+
 
 class Product(models.Model):
+    """
+    Model representing a product in the store.
+    """
     date = models.DateTimeField(auto_now_add=True)
-    #category_id = models.OneToOneField(Category, on_delete=models.CASCADE)
+    category = models.OneToOneField(Category, on_delete=models.CASCADE)
     stock = models.IntegerField()
     title = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     detail = models.JSONField()
     show = models.BooleanField(default=True)
-    image_id = models.ForeignKey(Image, on_delete=models.CASCADE)
-    currency_id = models.ForeignKey(Currency, on_delete=models.CASCADE)
-    #admin_id = models. ForeignKey(Admin, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    admin_name = models.CharField(max_length=150, null=True, blank=True)
+
+    def __str__(self):
+        """
+        Returns the name of the product as its string representation.
+        """
+        return str(self.title)
 
     class Meta:
+        """
+        Adds extra metadata to the Product model.
+        """
         verbose_name = "Product"
         verbose_name_plural = "Products"
-        abstract = True
+
+    @staticmethod
+    @login_required
+    def _get_admin_name_from_request():
+        return request.user.username
+
+    def save(self, *args, **kwargs):
+        if self.admin_name is None:
+            admin_name = self._get_admin_name_from_request()
+            if admin_name:
+                self.admin_name = admin_name
+        super(Product, self).save(*args, **kwargs)
