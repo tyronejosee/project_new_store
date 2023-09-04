638c1b951e5522a6d4adacf36b9290c818bbfbe2
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-09-04 13:37:21 -0300

    Added a global mixin to redirect the user to the login page.

--- products/views.py
+++ products/views.py
@@ -1,5 +1,18 @@
 """Views for Products App."""
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+
+
+class StaffRequiredMixin(object):
+    """
+    Mixin requirirá que el usuario sea staff o si no redirecciona a login de admin.
+    """
+    def dispatch(self, request, *args, **kwargs):
+        """Función principal del mixin"""
+        if not request.user.is_staff:
+            return redirect(reverse_lazy('admin:login'))
+        return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
 
 
 def products_main(request):
