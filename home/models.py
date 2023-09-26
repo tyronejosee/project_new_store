6af00c8eed178255bc488c725a28d995cd773b73
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-09-26 14:40:31 -0300

    Add CKEditor formatting to terms and about.

--- home/models.py
+++ home/models.py
@@ -1,6 +1,7 @@
 """Models for Home App."""
 
 from django.db import models
+from ckeditor.fields import RichTextField
 
 
 class Page(models.Model):
@@ -13,6 +14,14 @@ class Page(models.Model):
 
     category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
     title = models.CharField(max_length=200)
-    content = models.TextField()
+    content = RichTextField(blank=True, null=True, verbose_name='Content')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated at')
+
+    def __str__(self):
+        return str(self.title)
+
+    class Meta:
+        """Adds extra metadata to the Page model."""
+        verbose_name_plural = "Pages"
+        ordering = ['-created_at',]
