194d699d4543cb521e37a5874c647881c3852da7
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-10-05 11:49:32 -0300

    Update users_views

--- users/views.py
+++ users/views.py
@@ -6,23 +6,23 @@ from django.views.generic import CreateView
 from django.views.generic.edit import UpdateView
 from django.urls import reverse_lazy
 from django import forms
-from users.forms import UserCreationFormWithEmail, CustomUserForm, EmailForm
-from users.models import CustomUser
+from users.models import Profile
+from users.forms import UserCreationFormWithEmail, ProfileForm, EmailForm
 
 
 class SignUpView(CreateView):
     """View for user registration (Sign Up)."""
     form_class = UserCreationFormWithEmail
-    template_name = 'users/signup.html'
+    template_name = 'registration/signup.html'
 
     def get_success_url(self):
         """Get the URL to redirect to after a successful registration."""
-        return reverse_lazy('profile') + '?register'  
+        return reverse_lazy('login') + '?register'   
 
     def get_form(self, form_class=None):
         """Customize the appearance of the registration form fields."""
         form = super(SignUpView, self).get_form()
-        # Modify in real-time
+        # Modificar en tiempo real
         form.fields['username'].widget = forms.TextInput(
             attrs={'class':'form-control mb-2', 'placeholder':'Nombre de usuario'})
         form.fields['email'].widget = forms.EmailInput(
@@ -35,15 +35,15 @@ class SignUpView(CreateView):
 
 
 @method_decorator(login_required, name='dispatch')
-class CustomUserUpdate(UpdateView):
+class ProfileUpdate(UpdateView):
     """View for updating user profile information."""
-    form_class = CustomUserForm
+    form_class = ProfileForm
     success_url = reverse_lazy('profile')
-    template_name = 'users/profile_form.html'
+    template_name = 'registration/profile_form.html'
 
     def get_object(self):
         """Get the user's custom profile object for editing."""
-        profile, created = CustomUser.objects.get_or_create(user=self.request.user)
+        profile, created = Profile.objects.get_or_create(user=self.request.user)
         return profile
 
 
@@ -52,16 +52,16 @@ class EmailUpdate(UpdateView):
     """View for updating the user's email."""
     form_class = EmailForm
     success_url = reverse_lazy('profile')
-    template_name = 'users/profile_email_form.html'
+    template_name = 'registration/profile_email_form.html'
 
     def get_object(self):
         """Retrieve the user whose email is being edited."""
         return self.request.user
 
     def get_form(self, form_class=None):
-        """Customize the email form's appearance."""
         form = super(EmailUpdate, self).get_form()
         # Modify in real-time
         form.fields['email'].widget = forms.EmailInput(
             attrs={'class':'form-control mb-2', 'placeholder':'Email'})
         return form
+
