aedf44600ba9e73083e023bf246066bbfa99d0d9
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-10-05 11:50:49 -0300

    Update users_urls.

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,10 @@
 """URLs for Products App."""
 
 from django.urls import path
-from users.views import SignUpView, CustomUserUpdate, EmailUpdate
-
+from .views import SignUpView, ProfileUpdate, EmailUpdate
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name="signup"),
-    path('profile/', CustomUserUpdate.as_view(), name="profile"),
+    path('profile/', ProfileUpdate.as_view(), name="profile"),
     path('profile/email/', EmailUpdate.as_view(), name="profile_email"),
 ]
