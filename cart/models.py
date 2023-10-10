5f8a3c0478844bc956b3aa5e3c6cb56384e0f4cd
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-10-10 13:13:04 -0300

    Add a cart application and create the models.

---
+++ cart/models.py
@@ -0,0 +1,20 @@
+"""Models for Cart App."""
+
+from django.contrib.auth.models import User
+from django.db import models
+from products.models import Product
+
+
+class Cart(models.Model):
+    """Entity type model for Cart."""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ManyToManyField(Product, through='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class CartItem(models.Model):
+    """Pivot model for the shopping cart."""
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
