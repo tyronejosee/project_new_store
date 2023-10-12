999caacd435a0c30f610af676ad5f418710289a1
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-10-12 16:19:33 -0300

    Add views to the search bar.

--- products/urls.py
+++ products/urls.py
@@ -3,10 +3,10 @@
 from django.urls import path, include
 from products.views import ProductListView, ProductDetailView, product_search
 
-
 urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('catalog/', ProductListView.as_view(), name="products_list"),
-    path('catalog/item-<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('catalog/item-<int:pk>/',
+         ProductDetailView.as_view(), name='product_detail'),
     path('find/', product_search, name='product_search'),
 ]
