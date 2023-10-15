7cadaa29e0b6919d4963a8928c1fff0ed06fbe81
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-10-15 19:01:29 -0300

    Update the settings.py file, providing a more organized structure.

--- core/settings.py
+++ core/settings.py
@@ -4,25 +4,32 @@ from pathlib import Path
 import os
 import environ
 
-# Virtual ENV Settings
+
+# Base directory of the project
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Environment Variable settings
 env = environ.Env()
 environ.Env.read_env()
 ENVIRONMENT = env
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+
+# DEBUG settings: FALSE in production
 DEBUG = 'RENDER' not in os.environ
 
+
+# Host permission settings
 ALLOWED_HOSTS = [
     '*'
 ]
-
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
+
+# Installed Apps categories
 CORE_APPS = [
     'users',
     'core',
@@ -52,27 +59,38 @@ INSTALLED_APPS = CORE_APPS + DJANGO_APPS + THIRD_PARTY_APPS
 
 SITE_ID = 1
 
+
+# Login/Logout settings
 LOGIN_REDIRECT_URL = 'welcome'
 LOGOUT_REDIRECT_URL = 'products_list'
 
+
+# NPM
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+
+
+# Crispy Forms settings
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+CRISPY_TEMPLATE_PACK = "tailwind"
+
+
+# Django-tailwind settings
 TAILWIND_APP_NAME = 'theme'
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
-
-CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
-CRISPY_TEMPLATE_PACK = "tailwind"
-
+# CKEditor settings
 CKEDITOR_UPLOAD_PATH = "uploads/"
 CKEDITOR_IMAGE_BACKEND = "pillow"
+
 
 AUTHENTICATION_BACKENDS = [
     # Needed to log in by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
 ]
 
+# Middleware settings
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -83,8 +101,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# Root URL settings
 ROOT_URLCONF = 'core.urls'
 
+
+# Templates settings
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -102,16 +123,18 @@ TEMPLATES = [
     },
 ]
 
+# WSGI entry point
 WSGI_APPLICATION = 'core.wsgi.application'
 
+
+# Database settings
 DATABASES = {
     "default": env.db("DATABASE_URL", default="postgres:///new_store"),
 }
-
 DATABASES["default"]["ATOMIC_REQUEST"] = True
 
+
 PASSWORD_HASHERS = [
-    # https://docs.djangoproject.com/en/dev/topics/auth/passwords/#using-argon2-with-django
     "django.contrib.auth.hashers.Argon2PasswordHasher",
     "django.contrib.auth.hashers.PBKDF2PasswordHasher",
     "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
@@ -133,28 +156,29 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Internationalization settings
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
+
+# Static file settings
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
+
+# Media file settings
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+
+# AWS S3 settings
 if not DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = env('EMAIL_HOST')
@@ -175,8 +199,7 @@ if not DEBUG:
     # django-ckeditor will not work with S3 through django-storages without this line in settings.py
     AWS_QUERYSTRING_AUTH = False
 
-    # aws settings
-
+    # AWS settings
     AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
@@ -185,14 +208,12 @@ if not DEBUG:
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     AWS_DEFAULT_ACL = 'public-read'
 
-    # s3 static settings
-
+    # S3 static settings
     STATIC_LOCATION = 'static'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-    # s3 public media settings
-
+    # S3 public media settings
     PUBLIC_MEDIA_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
     DEFAULT_FILE_STORAGE = 'core.storage_backends.MediaStore'
