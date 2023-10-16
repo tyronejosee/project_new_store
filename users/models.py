c5a3adc2fc427254673d89ca83448e45fedd7d75
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-10-16 16:39:03 -0300

    Add the migrations and fix all the server startup errors.

--- users/models.py
+++ users/models.py
@@ -26,9 +26,10 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, username, email, first_name, last_name, password):
         custom_user = self.create_user(
             username=username,
-            email,  # Pending
+            email=email,
             first_name=first_name,
-            last_name=last_name
+            last_name=last_name,
+            password=password
         )
 
         custom_user.is_staff = True
@@ -39,13 +40,19 @@ class CustomUserManager(BaseUserManager):
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     """Entity type model for CustomUser."""
 
-    username = models.CharField(max_length=100, unique=True, verbose_name='Username')
-    email = models.EmailField(max_length=254, unique=True, verbose_name='Email')
-    first_name = models.CharField(max_length=200, blank=True, null=True, verbose_name='First Name')
-    last_name = models.CharField(max_length=200, blank=True, null=True, verbose_name='Last Name')
+    username = models.CharField(
+        max_length=100, unique=True, verbose_name='Username')
+    email = models.EmailField(
+        max_length=254, unique=True, verbose_name='Email')
+    first_name = models.CharField(
+        max_length=200, blank=True, null=True, verbose_name='First Name')
+    last_name = models.CharField(
+        max_length=200, blank=True, null=True, verbose_name='Last Name')
     address = models.TextField(blank=True, verbose_name='Adress')
-    phone_number = models.CharField(max_length=15, blank=True, verbose_name='Phone Number')
-    date_joined = models.DateTimeField(default=timezone.now, verbose_name='Date Joined')
+    phone_number = models.CharField(
+        max_length=15, blank=True, verbose_name='Phone Number')
+    date_joined = models.DateTimeField(
+        default=timezone.now, verbose_name='Date Joined')
     is_active = models.BooleanField(default=True, verbose_name='Is Active')
     is_staff = models.BooleanField(default=False, verbose_name='Is Staff')
 
@@ -63,7 +70,5 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     def has_module_perms(self, app_label):
         return True
 
-    @property
     def is_staff(self):
-        """Property to check if the user is staff."""
         return self.is_staff
