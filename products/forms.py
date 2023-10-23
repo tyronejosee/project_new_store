1f95b8adb133057e73d175a9fd8ff0aa88cc2190
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-10-23 12:43:53 -0300

    Add the form for product filtering in categories.

--- products/forms.py
+++ products/forms.py
@@ -1,3 +1,45 @@
 """Forms for Products App."""
 
 from django import forms
+from .models import Category, Brand, Deal
+
+
+class CategoriesForm(forms.Form):
+    """Base form for category filtering."""
+
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        required=False,
+        empty_label="All Categories",
+        widget=forms.Select(attrs={
+            'class': 'border p-2 text-gray-700 rounded-lg focus:ring focus:ring-blue-300 focus:outline-none w-full'})
+    )
+    brand = forms.ModelChoiceField(
+        queryset=Brand.objects.all(),
+        required=False,
+        empty_label="All Brands",
+        widget=forms.Select(attrs={
+            'class': 'border p-2 text-gray-700 rounded-lg focus:ring focus:ring-blue-300 focus:outline-none w-full'})
+    )
+    deal = forms.ModelChoiceField(
+        queryset=Deal.objects.all(),
+        required=False,
+        empty_label="All Deals",
+        widget=forms.Select(attrs={
+            'class': 'border p-2 text-gray-700 rounded-lg focus:ring focus:ring-blue-300 focus:outline-none w-full'})
+
+    )
+    min_price = forms.DecimalField(
+        required=False,
+        min_value=0,
+        max_value=10000,
+        widget=forms.NumberInput(attrs={
+            'class': 'border p-2 text-gray-700 rounded-lg focus:ring focus:ring-blue-300 focus:outline-none w-full'})
+    )
+    max_price = forms.DecimalField(
+        required=False,
+        min_value=0,
+        max_value=10000,
+        widget=forms.NumberInput(attrs={
+            'class': 'border p-2 text-gray-700 rounded-lg focus:ring focus:ring-blue-300 focus:outline-none w-full'})
+    )
