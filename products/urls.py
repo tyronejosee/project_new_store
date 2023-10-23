209e0556f8208e06ec8b95896f9c3a68b27874d8
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-10-23 13:16:27 -0300

    Add views, forms, URLs, and templates for Categories, Update product templates.

--- products/urls.py
+++ products/urls.py
@@ -2,15 +2,20 @@
 
 from django.urls import path, include
 from products.views import (
-    ProductListView, ProductDetailView, RecentProductsListView, product_search
+    ProductListView, ProductDetailView, RecentProductsListView, product_search, CategoriesListView
 )
 
 
 urlpatterns = [
+    # Others
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('catalog/', ProductListView.as_view(), name="products_list"),
-    path('catalog/item-<int:pk>/',
-         ProductDetailView.as_view(), name='product_detail'),
+
+    # Public paths
+    path('categories/', CategoriesListView.as_view(), name='categories'),
+    path('all/', ProductListView.as_view(), name="products_list"),
     path('recent/', RecentProductsListView.as_view(), name="recent"),
+    path('item-<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('find/', product_search, name='product_search'),
+
+    # Private paths
 ]
--- products/views.py
+++ products/views.py
@@ -4,12 +4,13 @@ from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 from django.db.models import Q
 from products.models import Product
+from products.forms import CategoriesForm
 
 
 class ProductListView(ListView):
-    """ListView for displaying active products with stock greater than or equal to 1."""
+    """Display the complete list of all products, active and in stock."""
     model = Product
-    template_name = 'products/product_list.html'
+    template_name = 'products/list.html'
     context_object_name = 'products'
     queryset = Product.objects.filter(show_hide=True, stock__gte=1)
     paginate_by = 8
@@ -17,17 +18,55 @@ class ProductListView(ListView):
 
 
 class ProductDetailView(DetailView):
-    """DetailView that expands product information."""
+    """Display the details of a specific product."""
     model = Product
-    template_name = 'products/product_detail.html'
+    template_name = 'products/detail.html'
     context_object_name = 'product'
     pk_url_kwarg = 'pk'
 
 
-class RecentProductsListView(ListView):
-    """View to display a list of recent products."""
+class CategoriesListView(ListView):
+    """Display a list of products filtered by categories."""
+
     model = Product
-    template_name = 'products/recent_products.html'
+    template_name = 'products/categories.html'
+    context_object_name = 'products'
+    paginate_by = 18
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = CategoriesForm()
+        return context
+
+    def get_queryset(self):
+        queryset = Product.objects.filter(show_hide=True, stock__gte=1)
+        form = CategoriesForm(self.request.GET)
+
+        if form.is_valid():
+            category = form.cleaned_data['category']
+            brand = form.cleaned_data['brand']
+            deal = form.cleaned_data['deal']
+            min_price = form.cleaned_data['min_price']
+            max_price = form.cleaned_data['max_price']
+
+            if category:
+                queryset = queryset.filter(category=category)
+            if brand:
+                queryset = queryset.filter(brand=brand)
+            if deal:
+                queryset = queryset.filter(productdeal__deal=deal)
+            if min_price:
+                queryset = queryset.filter(normal_price__gte=min_price)
+            if max_price:
+                queryset = queryset.filter(normal_price__lte=max_price)
+
+        return queryset
+
+
+class RecentProductsListView(ListView):
+    """Display a list of recent products."""
+    model = Product
+    template_name = 'products/recent.html'
     context_object_name = 'products'
     paginate_by = 8
     ordering = ['-created_at', 'title']
@@ -42,4 +81,4 @@ def product_search(request):
             Q(title__icontains=queryset) |
             Q(brand__name__icontains=queryset)
         ).distinct()
-    return render(request, 'products/product_search.html', {'products': products})
+    return render(request, 'products/search_bar.html', {'products': products})
