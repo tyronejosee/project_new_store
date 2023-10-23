1f7484b4a2df6d86c1c2033593c3eb8a2bf92c4f
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-10-23 19:26:19 -0300

    Add app_name to products for better comprehension on the routes.

--- products/urls.py
+++ products/urls.py
@@ -5,18 +5,17 @@ from products.views import (
     ProductListView, ProductDetailView, RecentProductsListView, product_search, CategoriesListView, BrandListView
 )
 
+app_name = 'products'
 
 urlpatterns = [
     # Others
     path('ckeditor/', include('ckeditor_uploader.urls')),
 
     # Public paths
-    path('', product_search, name='product_search'),
+    path('', product_search, name='search'),
     path('categories/', CategoriesListView.as_view(), name='categories'),
-    path('items/', ProductListView.as_view(), name="products_list"),
-    path('items/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-    path('brands/<str:brand_name>/', BrandListView.as_view(), name='brand'),
+    path('items/', ProductListView.as_view(), name='items'),
+    path('items/<int:pk>/', ProductDetailView.as_view(), name='detail'),
+    path('brands/<str:brand_name>/', BrandListView.as_view(), name='brands'),
     path('recent/', RecentProductsListView.as_view(), name="recent"),
-
-    # Private paths
 ]
