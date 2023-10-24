c21b10b6f3744cf763ceea54119446a49d1577c6
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-10-24 13:54:23 -0300

    Update the app_name in home, Fix all broken routes.

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,8 @@
 from django.urls import path
 from home.views import PageDetailView, FeaturedProductListView
 
+app_name = 'home'
+
 urlpatterns = [
     path("", FeaturedProductListView.as_view(), name="featured_product"),
     path("company/<str:key>/", PageDetailView.as_view(), name="page_detail"),
