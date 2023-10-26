e63cb4eb1da937abd16d58d5cdbfae5f78a6fdda
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-10-26 12:07:56 -0300

    Add CRUD for product management, pending add restrictions to the views.

--- management/urls.py
+++ management/urls.py
@@ -1,10 +1,17 @@
 """URLs for Management App."""
 
 from django.urls import path
-from management.views import ProductListView
+from management.views import (
+    ProductListView, ProductCreateView, ProductUpdateView, ProductDeleteView
+)
 
 app_name = 'management'
 
 urlpatterns = [
-    path('products/list/', ProductListView.as_view(), name='product_list'),
+    path('products/list/', ProductListView.as_view(), name='prod_list'),
+    path('products/create/', ProductCreateView.as_view(), name='prod_create'),
+    path('products/update/<int:pk>',
+         ProductUpdateView.as_view(), name='prod_update'),
+    path('products/delete/<int:pk>',
+         ProductDeleteView.as_view(), name='prod_delete'),
 ]
--- management/views.py
+++ management/views.py
@@ -1,20 +1,13 @@
 """Views for Management App."""
 
-from django.contrib.auth.decorators import user_passes_test
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
 from products.models import Product
+from management.forms import ProductForm
 
 
-def is_admin(user):
-    """
-    Check if the user is an administrator.
-    Returns True if the user is an administrator (is_staff).
-    """
-    return user.is_staff
-
-
-@user_passes_test(is_admin)
 class ProductListView(ListView):
     """Display a list of available products."""
 
@@ -24,41 +17,40 @@ class ProductListView(ListView):
     paginate_by = 18
 
     def get_queryset(self):
-        """
-        Select specific fields from the 'Product' model using the 'only' method
-        """
+        """Select specific fields from the 'Product' model using the 'only' method"""
         return Product.objects.only(
             'title', 'normal_price', 'image', 'stock',
             'featured', 'show_hide'
         )
 
 
-@user_passes_test(is_admin)
 class ProductCreateView(CreateView):
-    """Pending."""
+    """Create a new product."""
 
     model = Product
-    form_class = None
-    template_name = None
-    success_url = None
+    form_class = ProductForm
+    template_name = 'management/product_form.html'
+    success_url = reverse_lazy('management:prod_list')
 
 
-@user_passes_test(is_admin)
 class ProductUpdateView(UpdateView):
-    """Pending."""
+    """Update the information of a product."""
 
     model = Product
-    form_class = None
-    template_name = None
-    success_url = None
+    form_class = ProductForm
+    template_name = 'management/product_form.html'
+    success_url = reverse_lazy('management:prod_list')
 
 
-@user_passes_test(is_admin)
 class ProductDeleteView(DeleteView):
-    """Pending."""
+    """Remove an item from the products logically."""
 
     model = Product
     success_url = None
 
     def post(self, request, pk, *args, **kwargs):
-        pass
+        """Redefine the POST method for logically deleting the product."""
+        object = Product.objects.get(id=pk)
+        object.show_hide = False
+        object.save()
+        return redirect('management:prod_list')
