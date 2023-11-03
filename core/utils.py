96c8a54349163729fa1ab052ee1a0b3a5a8fe7b9
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-11-03 12:42:03 -0300

    Add utils in the forms to avoid code repetition.

--- core/utils.py
+++ core/utils.py
@@ -1,7 +1,7 @@
-"""Utils for Users App."""
+"""Global Utils."""
 
 
-def get_common_widget_attrs(placeholder):
+def placeholder_attrs(placeholder):
     """Returns an HTML attribute dictionary to avoid code repetition."""
     return {
         'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-3xl focus:ring-sky-500 focus:border-sky-500 block w-full p-2.5 dark:bg-neutral-800 dark:border-neutral-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500',
--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,7 @@
 
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
-from users.util import get_common_widget_attrs
+from core.utils import placeholder_attrs
 from users.models import CustomUser
 
 
@@ -11,9 +11,9 @@ class UserLoginForm(AuthenticationForm):
 
     def __init__(self, *args, **kwargs):
         super(UserLoginForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs['class'] = 'shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light'
+        self.fields['username'].widget.attrs = placeholder_attrs('Username')
         self.fields['username'].widget.attrs['placeholder'] = 'Username'
-        self.fields['password'].widget.attrs['class'] = 'shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light'
+        self.fields['password'].widget.attrs = placeholder_attrs('Password')
         self.fields['password'].widget.attrs['placeholder'] = 'Password'
 
 
@@ -21,11 +21,11 @@ class UserRegistrationForm(forms.ModelForm):
     """Base form for user registration."""
 
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput(
-        attrs=get_common_widget_attrs('Password')
+        attrs=placeholder_attrs('Password')
     ))
 
     password2 = forms.CharField(label='Repeat your password', widget=forms.PasswordInput(
-        attrs=get_common_widget_attrs('Repeat your password')
+        attrs=placeholder_attrs('Repeat your password')
     ))
 
     class Meta:
@@ -34,12 +34,12 @@ class UserRegistrationForm(forms.ModelForm):
         fields = ('username', 'email', 'first_name',
                   'last_name', 'address', 'phone_number')
         widgets = {
-            'username': forms.TextInput(attrs=get_common_widget_attrs('Username')),
-            'email': forms.EmailInput(attrs=get_common_widget_attrs('Email')),
-            'first_name': forms.TextInput(attrs=get_common_widget_attrs('First Name')),
-            'last_name': forms.TextInput(attrs=get_common_widget_attrs('Last Name')),
-            'address': forms.TextInput(attrs=get_common_widget_attrs('Address')),
-            'phone_number': forms.TextInput(attrs=get_common_widget_attrs('Phone Number')),
+            'username': forms.TextInput(attrs=placeholder_attrs('Username')),
+            'email': forms.EmailInput(attrs=placeholder_attrs('Email')),
+            'first_name': forms.TextInput(attrs=placeholder_attrs('First Name')),
+            'last_name': forms.TextInput(attrs=placeholder_attrs('Last Name')),
+            'address': forms.TextInput(attrs=placeholder_attrs('Address')),
+            'phone_number': forms.TextInput(attrs=placeholder_attrs('Phone Number')),
         }
 
     def clean_password2(self):
