59e215b992cb66eee8db2312782cf7dd620a1594
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-11-04 18:33:19 -0300

    Update home views to send multiple contexts.

--- home/views.py
+++ home/views.py
@@ -6,7 +6,7 @@ from products.models import Product
 
 
 class IndexTemplateView(TemplateView):
-    """Renders the site's landing page."""
+    """Renders the site's index with multiple contexts."""
     template_name = 'home/index.html'
 
     def get_context_data(self, **kwargs):
@@ -17,15 +17,19 @@ class IndexTemplateView(TemplateView):
         context['recent_products'] = Product.objects.filter(
             show_hide=True, stock__gte=1
         ).order_by('-updated_at')[:12]
+        context['iphone'] = Product.objects.filter(
+            show_hide=True, stock__gte=1, category__title__iexact='iphone'
+        ).order_by('-updated_at')[:12]
         return context
 
 
 class PageDetailView(DetailView):
-    """Pending."""
+    """Display details of the static site pages."""
     model = Page
     template_name = 'home/page_detail.html'
     context_object_name = 'page'
 
     def get_object(self, queryset=None):
+        """Gets a key from the HTML and uses it as a filter for searching in the database."""
         key = self.kwargs['key']
         return Page.objects.get(key=key)
