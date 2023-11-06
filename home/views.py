4cd6f7738a6fc4b91c728561adf4caaa20a80bac
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-11-06 17:03:42 -0300

    Update views and templates of the Home app.

--- home/views.py
+++ home/views.py
@@ -10,21 +10,40 @@ class IndexTemplateView(TemplateView):
     template_name = 'home/index.html'
 
     def get_context_data(self, **kwargs):
+        """Override the get_context_data method to send multiple contexts."""
         context = super().get_context_data(**kwargs)
+
+        # Send all the products (first 12 prods.)
         context['all_products'] = Product.objects.filter(
             show_hide=True, stock__gte=1
         ).order_by('title')[:12]
+
+        # Send recent products (first 12 prods.)
         context['recent_products'] = Product.objects.filter(
             show_hide=True, stock__gte=1
         ).order_by('-updated_at')[:12]
-        context['iphone'] = Product.objects.filter(
-            show_hide=True, stock__gte=1, category__title__iexact='iphone'
-        ).order_by('-updated_at')[:12]
+
+        # Send products from the apple brand (first 6 prods.)
+        context['apple'] = Product.objects.filter(
+            show_hide=True, stock__gte=1, brand__name__iexact='apple'
+        ).order_by('-updated_at')[:6]
+
+        # Send products from the samsung brand (first 6 prods.)
+        context['samsung'] = Product.objects.filter(
+            show_hide=True, stock__gte=1, brand__name__iexact='samsung'
+        ).order_by('-updated_at')[:6]
+
+        # Send products from the computer monitors category (first 6 prods.)
+        context['computer_monitors'] = Product.objects.filter(
+            show_hide=True, stock__gte=1, category__title__iexact='computer monitors'
+        ).order_by('-updated_at')[:6]
+
         return context
 
 
 class PageDetailView(DetailView):
     """Display details of the static site pages."""
+
     model = Page
     template_name = 'home/page_detail.html'
     context_object_name = 'page'
