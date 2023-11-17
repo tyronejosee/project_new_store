bac103f766b089f48be65939f4ec2dedd1074679
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-11-17 17:49:21 -0300

    Update registration and login forms.

--- core/utils.py
+++ core/utils.py
@@ -4,6 +4,6 @@
 def placeholder_attrs(placeholder):
     """Returns an HTML attribute dictionary to avoid code repetition."""
     return {
-        'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-3xl focus:ring-sky-500 focus:border-sky-500 block w-full p-2.5 dark:bg-neutral-800 dark:border-neutral-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500',
+        'class': 'bg-neutral-100 p-4 text-neutral-800 rounded-xl block w-full dark:bg-neutral-900 dark:text-neutral-200 dark:focus:ring-sky-500 dark:focus:border-sky-500',
         'placeholder': placeholder,
     }
