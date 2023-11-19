f1723a8ddda474e6a1e386c1d51e2cf2bf9da2e4
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-11-19 17:06:13 -0300

    Update jQuery code separate from the HTML.

---
+++ static/js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    $('.show-image-popup').on('click', function(e) {
+      e.preventDefault();
+      var imageUrl = $(this).data('src');
+      var modalImg = $('#expandedImage');
+      modalImg.attr('src', imageUrl);
+      $('#imagePopup').removeClass('hidden');
+    });
+  
+    $('#closeImagePopup, .modal-overlay').on('click', function() {
+      $('#imagePopup').addClass('hidden');
+    });
+  
+    $("#scrollToTop").click(function() {
+      $("html, body").animate(
+        {
+          scrollTop: 0
+        },
+        500
+      );
+    });
+
+    $("#user-menu-button").on("click", function () {
+      var $userDropdown = $("#user-dropdown");
+      if ($userDropdown.is(":hidden")) {
+        $userDropdown.slideDown("fast");
+      } else {
+        $userDropdown.slideUp("fast");
+      }
+    });
+
+    $("#product-tabs a").click(function (e) {
+      e.preventDefault();
+      $(".tab-content").addClass("hidden");
+      $($(this).attr("href")).removeClass("hidden");
+    });
+  });
