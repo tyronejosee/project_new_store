62f15a4abe450a4a6956b90c3e47829dcb96b9bd
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-11-25 17:46:19 -0300

    Update custom component library.

--- core/utils.py
+++ core/utils.py
@@ -3,21 +3,21 @@
 def form_text_input(placeholder):
     """Utility classes for text input."""
     return {
-        'class': 'bg-neutral-100 p-4 text-neutral-800 rounded-xl block w-full dark:bg-neutral-900 dark:text-neutral-200 dark:focus:ring-sky-500 dark:focus:border-sky-500',
+        'class': 'bg-neutral-100 p-4 text-neutral-800 cs-rounded block w-full dark:bg-neutral-900 dark:text-neutral-200 dark:focus:ring-sky-500 dark:focus:border-sky-500',
         'placeholder': placeholder,
     }
 
 def form_number_input(placeholder):
     """Utility classes for number input."""
     return {
-        'class': 'bg-neutral-100 p-4 text-neutral-800 rounded-xl block w-full dark:bg-neutral-900 dark:text-neutral-200 dark:focus:ring-sky-500 dark:focus:border-sky-500',
+        'class': 'bg-neutral-100 p-4 text-neutral-800 cs-rounded block w-full dark:bg-neutral-900 dark:text-neutral-200 dark:focus:ring-sky-500 dark:focus:border-sky-500',
         'placeholder': placeholder,
     }
 
 def form_checkbox_input():
     """Utility classes for checkbox input."""
     return {
-        'class': 'w-4 h-4 border border-gray-300 rounded-xl dark:bg-primary focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800'
+        'class': 'w-4 h-4 border border-gray-300 cs-rounded dark:bg-primary focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800'
     }
 
 def form_file_input():
@@ -29,11 +29,11 @@ def form_file_input():
 def form_select():
     """Utility classes for select."""
     return {
-        'class': 'bg-neutral-100 dark:bg-neutral-900 w-full p-2 rounded-xl p-4',
+        'class': 'bg-neutral-100 dark:bg-neutral-900 w-full p-2 cs-rounded p-4',
     }
 
 def form_textarea():
     """Utility classes for text area."""
     return {
-        'class': 'bg-neutral-100 dark:bg-neutral-900 w-full p-2 rounded-xl p-4',
+        'class': 'bg-neutral-100 dark:bg-neutral-900 w-full p-2 cs-rounded p-4',
     }
