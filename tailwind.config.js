ce379904c1139ede1afd46df4789e90986069a61
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-11-26 17:47:25 -0300

    Update custom component library.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,18 @@ module.exports = {
         primary: '#dc2626',
         primaryHover: '#b91c1c',
         primaryFocus: '#fca5a5',
+
         secondary: '#e5e5e5',
         secondaryHover: '#d4d4d4',
         secondaryFocus: '#f5f5f5',
+
+        light: '#f5f5f5',
+        lightTab: '#FFFFFF',
+        lightHover: '#e5e5e5',
+
+        dark: '#171717',
+        darkTab: '#292524',
+        darkHover: '#404040',
       },
     },
   },
