d86f3ce1cd2eca06b7aa2e7edb112d3e099c790e
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-11-27 18:05:33 -0300

    Update header animation.

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,32 @@
 $(document).ready(function() {
+
+    var containerTimeout;
+    var dropdownTimeout;
+
     $('#dropdownContainer').mouseenter(function() {
-      $('#dropdown').slideDown(400);
+        // Establecer un temporizador para el contenedor (por ejemplo, 500 ms)
+        containerTimeout = setTimeout(function() {
+            // La animación del contenedor se activa solo después de que el mouse ha estado en el área durante 500 ms
+            $('#dropdown').stop(true, true).slideDown(250);
+        }, 250);
     });
 
-    $('#dropdownContainer').mouseleave(function() {
-      $('#dropdown').slideUp(400);
+    $('#dropdownContainer, #dropdown').mouseleave(function() {
+        clearTimeout(containerTimeout);
+    });
+
+    $('#dropdown').mouseenter(function() {
+        clearTimeout(containerTimeout);
+    });
+
+    $('#dropdown').mouseleave(function() {
+        dropdownTimeout = setTimeout(function() {
+            $('#dropdown').stop(true, true).slideUp(250);
+        }, 250);
+    });
+
+    $('#dropdown').mouseenter(function() {
+        clearTimeout(dropdownTimeout);
     });
 
     $('.show-image-popup').on('click', function(e) {
