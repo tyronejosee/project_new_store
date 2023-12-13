08b272ecf9b876b854c4b1ef34e0808a063576e8
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-12-13 15:14:00 -0300

    Add home urls test.

--- home/tests/test_urls.py
+++ home/tests/test_urls.py
@@ -1,1 +1,23 @@
 """URLs Tests for the Home App."""
+
+from django.test import TestCase
+from django.urls import reverse, resolve
+from home.views import IndexTemplateView, PageDetailView
+
+
+class HomeUrlsTest(TestCase):
+    """Tests for Home URLs."""
+
+    def test_index_url(self):
+        """"Resolves 'search' URL and view."""
+        url = reverse('home:index')
+        resolver = resolve(url)
+        # Expected responses
+        self.assertEqual(resolver.func.view_class, IndexTemplateView)
+
+    def test_page_detail_url(self):
+        """"Resolves 'page_detail' URL and view."""
+        url = reverse('home:page_detail', args=['key_example'])
+        resolver = resolve(url)
+        # Expected responses
+        self.assertEqual(resolver.func.view_class, PageDetailView)
