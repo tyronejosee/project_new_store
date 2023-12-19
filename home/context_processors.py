4a2712d21686eefbcbd4c399fd7278679122f2c0
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-12-19 14:37:27 -0300

    Add context processors in utils.

--- home/context_processors.py
+++
@@ -1,9 +0,0 @@
-"""Context Processors for Home App."""
-
-from home.models import Company
-
-
-def company_context(request):
-    """Context processors for company data."""
-    company = Company.objects.first()
-    return {'company': company}
--- utils/context_processors.py
+++ utils/context_processors.py
@@ -1,9 +1,29 @@
 """Utils: Context Processors"""
 
+from home.models import Company
+from products.models import Product, Category
 from cart.models import Cart
 
 
-def cart_items_context(request):
+def company(_request):
+    """Context processor for company data."""
+    company_data = Company.objects.first()
+    return {'company': company_data}
+
+
+def products_featured(_request):
+    """Context processor return a list of featured products."""
+    products_featured_list = Product.objects.filter(featured=True, show_hide=True, stock__gte=1)[:6]
+    return {'products_featured': products_featured_list}
+
+
+def products_categories(_request):
+    """Context processor to provide a list of categories."""
+    products_categories_list = Category.objects.filter(show_hide=True)
+    return {'products_categories': products_categories_list}
+
+
+def cart_items_count(request):
     """Context processor to add the cart items count to the context."""
     user = request.user
 
@@ -11,6 +31,6 @@ def cart_items_context(request):
     cart, created = Cart.objects.get_or_create(user=user)
 
     # Calculate the total quantity of items in the cart
-    cart_items_count = sum(item.quantity for item in cart.cart_items.all())
+    cart_items_count_list = sum(item.quantity for item in cart.cart_items.all())
 
-    return {'cart_items_count': cart_items_count}
+    return {'cart_items_count': cart_items_count_list}
