c37722fc2b6b317fd49a1f87e06fe644a3c90e68
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-01-10 16:02:51 -0300

    Remove custom colors and all their dependencies.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,20 +7,6 @@ module.exports = {
     extend: {
       colors: {
         primary: '#dc2626',
-        primaryHover: '#b91c1c',
-        primaryFocus: '#fca5a5',
-
-        secondary: '#e5e5e5',
-        secondaryHover: '#d4d4d4',
-        secondaryFocus: '#f5f5f5',
-
-        light: '#f5f5f5', /** n 100 */
-        lightTab: '#FFFFFF', /** white */
-        lightHover: '#e5e5e5', /** n 200 */
-
-        dark: '#171717', /** n 900 */
-        darkTab: '#262626', /** n 800 */
-        darkHover: '#404040', /** n 700 */
       },
       fontFamily: {
         body: ['Poppins']
