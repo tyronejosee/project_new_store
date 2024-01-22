f7f517a9a899f250ba056e48ac6d40301579539e
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-01-22 15:00:04 -0300

    Add tests to home app models.

--- home/tests/test_models.py
+++ home/tests/test_models.py
@@ -1,1 +1,69 @@
 """Models Tests for the Home App."""
+
+from django.test import TestCase
+from home.models import Company, Page
+
+class CompanyModelTest(TestCase):
+    """Test cases for the Company model."""
+
+    def setUp(self):
+        """Set up initial data for tests."""
+        self.company_data = {
+            'name': 'Example Company',
+            'logo': 'company/logo.png',
+            'copy': 'Example Copy',
+            'description': 'Example Description',
+            'email': 'example@example.com',
+            'github': 'https://github.com/example',
+            'linkedin': 'https://linkedin.com/example'
+        }
+
+    def test_create_instance(self):
+        """Test creating a Company instance."""
+        company = Company.objects.create(**self.company_data)
+        self.assertEqual(company.name, 'Example Company')
+        self.assertEqual(company.description, 'Example Description')
+        self.assertNotEqual(company.name, 'Example Error')
+
+    def test_company_str_representation(self):
+        """Test the string representation of a Company instance."""
+        company = Company.objects.create(name='Example Company')
+        self.assertEqual(str(company), 'Example Company')
+
+    def test_update_company_instance(self):
+        """Test updating a Company instance."""
+        company = Company.objects.create(name='Old Company Name')
+        company.name = 'New Company Name'
+        company.save()
+        self.assertEqual(company.name, 'New Company Name')
+        self.assertNotEqual(company.name, 'Old Company Name')
+
+
+class PageModelTest(TestCase):
+    """Test cases for the Page model."""
+
+    def setUp(self):
+        """Set up initial data for tests."""
+        self.page_data = {
+            'key': 'example_page',
+            'content': 'Example Content',
+            'image': 'pages/example_image.png',
+        }
+
+    def test_create_page_instance(self):
+        """Test creating a Page instance."""
+        page = Page.objects.create(**self.page_data)
+        self.assertEqual(page.key, 'example_page')
+
+    def test_page_str_representation(self):
+        """Test the string representation of a Page instance."""
+        page = Page.objects.create(key='example_page')
+        self.assertEqual(str(page), 'example_page')
+
+    def test_update_page_instance(self):
+        """Test updating a Page instance."""
+        page = Page.objects.create(key='Old Key')
+        page.key = 'New Key'
+        page.save()
+        self.assertEqual(page.key, 'New Key')
+        self.assertNotEqual(page.key, 'Old Key')
--- management/tests/test_models.py
+++
@@ -1,1 +0,0 @@
-"""Models Tests for the Management App."""
