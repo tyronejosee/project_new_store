f8cf05287b9edf9a9947d5738a7e113ae4184b80
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-01-31 13:32:44 -0300

    Update tests for payment and products for better understanding.

--- payment/tests/test_views.py
+++ payment/tests/test_views.py
@@ -9,16 +9,14 @@ class PaymentViewsTest(BaseTestCase):
 
     def test_payment_success_view(self):
         """Verifies the functionality of the 'payment_success' view."""
-        response = self.client.get(reverse(
-            'payment:payment_success', kwargs={'product_id': self.product.id})
-        )
+        url = reverse('payment:payment_success', kwargs={'product_id': self.product.id})
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context['product'], self.product)
 
     def test_payment_failed_view(self):
         """Verifies the functionality of the 'payment_failed' view."""
-        response = self.client.get(reverse(
-            'payment:payment_failed', kwargs={'product_id': self.product.id})
-        )
+        url = reverse('payment:payment_failed', kwargs={'product_id': self.product.id})
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context['product'], self.product)
--- products/tests/test_views.py
+++ products/tests/test_views.py
@@ -53,75 +53,86 @@ class ProductViewsTest(TestCase):
 
     def test_product_list_view(self):
         """Test for ProductListView."""
-        response = self.client.get(reverse('products:prod_list'))
+        url = reverse('products:prod_list')
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/product_list.html')
         self.assertContains(response, 'All Products')
 
     def test_product_detail_view(self):
         """Test for ProductDetailView."""
-        response = self.client.get(reverse('products:detail', args=[self.product.id]))
+        url = reverse('products:detail', args=[self.product.id])
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/product_detail.html')
         self.assertContains(response, 'Test Product')
 
     def test_categories_list_view(self):
         """Test for CategoriesListView."""
-        response = self.client.get(reverse('products:categories'))
+        url = reverse('products:categories')
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/product_list.html')
         self.assertContains(response, 'Categories')
 
     def test_deal_list_view(self):
         """Test for DealListView."""
-        response = self.client.get(reverse('products:deal_list'))
+        url = reverse('products:deal_list')
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/deal_list.html')
 
     def test_deal_detail_view(self):
         """Test for DealDetailView."""
-        response = self.client.get(reverse('products:deal_detail', args=[self.deal.slug]))
+        url = reverse('products:deal_detail', args=[self.deal.slug])
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/deal_detail.html')
         self.assertContains(response, 'Test Deal')
 
     def test_recent_products_list_view(self):
         """Test for RecentProductsListView."""
-        response = self.client.get(reverse('products:recent'))
+        url = reverse('products:recent')
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/product_list.html')
         self.assertContains(response, 'Recent Products')
 
     def test_category_filter_list_view(self):
         """Test for CategoryFilterListView."""
-        response = self.client.get(reverse('products:category_filter', args=[self.category.slug]))
+        url = reverse('products:category_filter', args=[self.category.slug])
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/product_list.html')
         self.assertContains(response, 'Test Category')
 
     def test_brand_filter_list_view(self):
         """Test for BrandFilterListView."""
-        response = self.client.get(reverse('products:brand_filter', args=[self.brand.slug]))
+        url = reverse('products:brand_filter', args=[self.brand.slug])
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/product_list.html')
         self.assertContains(response, 'Test Brand')
 
     def test_product_search_view(self):
         """Test for product_search."""
-        response = self.client.get(reverse('products:search'), {'search': 'Test'})
+        url = reverse('products:search')
+        response = self.client.get(url, {'search': 'Test'})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/product_list.html')
         self.assertContains(response, 'Test Product')
 
     def test_product_search_view_empty_query(self):
         """Test for product_search with empty query."""
-        response = self.client.get(reverse('products:search'))
+        url = reverse('products:search')
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/product_list.html')
 
     def test_product_search_view_with_results(self):
         """Test for product_search with results."""
-        response = self.client.get(reverse('products:search'), {'search': 'Test'})
+        url = reverse('products:search')
+        response = self.client.get(url, {'search': 'Test'})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/product_list.html')
         self.assertContains(response, 'Test Product')
