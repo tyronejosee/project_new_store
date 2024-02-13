d17bb581a7801c75963fbeb27595856cd2b8b715
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-02-13 13:15:17 -0300

    Fix custom command for Render v2.

--- core/management/commands/render.py
+++ core/management/commands/render.py
@@ -11,18 +11,12 @@ class Command(BaseCommand):
     """Command to create a superuser if 'admin' does not exist."""
 
     def handle(self, *args, **options):
-        username = 'newstore'
-        email = 'newstore@admin.com'
-        first_name = 'new'
-        last_name = 'store'
-        password = 'newstoreadmin'
-
-        user_admin = User.objects.create_superuser(
-            username=username,
-            email=email,
-            first_name=first_name,
-            last_name=last_name,
-            password=password
-        )
-
-        self.stdout.write(self.style.SUCCESS(f'Superuser "{username}" created successfully.'))
+        user_admin = User.objects.filter(username='newstore')
+        if not user_admin:
+            user_admin = User.objects.create_superuser(
+                username = 'newstore',
+                email = 'newstore@admin.com',
+                first_name = 'new',
+                last_name = 'store',
+                password = 'newstoreadmin'
+            )
