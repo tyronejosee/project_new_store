bf21ee3b636303c694aea323f63325ea3eb1aad1
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-02-26 17:26:42 -0300

    Tests for render.com

--- core/settings.py
+++ core/settings.py
@@ -155,6 +155,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
